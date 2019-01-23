4fe55a34e3a9fac3e5fe7fa197ce77703df5be76
Dorian Baudry <dobaudry19@gmail.com>
2019-01-23 20:42:28 +0100

    correction in prediction function + add metrics

--- Instance_learning.py
+++ Instance_learning.py
@@ -29,8 +29,12 @@ class learning_instance_preference:
     def gaussian_kernel(x, y, K):
         return np.exp(-K/2.*np.sum((x-y)**2))
 
-    def compute_cov(self,K):
-        cov=np.eye(self.n)
+    def compute_cov(self, K):
+        """
+        :param K: parameter of the gaussian kernel
+        :return: Covariance matrix for the training dataset
+        """
+        cov = np.eye(self.n)
         for i in range(self.n):
             for j in range(i):
                 cov_ij = self.gaussian_kernel(self.X[i], self.X[j], K)
@@ -39,32 +43,52 @@ class learning_instance_preference:
         return cov
 
     def compute_prior(self, y):
+        """
+        :param y: vector of size self.n
+        :return: prior density in y
+        """
         return mn.pdf(y, mean=np.zeros(self.n), cov=self.cov)
 
-    def compute_z_phi(self,y):
-        z=np.apply_along_axis(lambda x:(y[x[0]]-y[x[1]])/(np.sqrt(2)*self.sigma), 1, self.D)
+    def compute_z_phi(self, y):
+        """
+        :param y: vector of size n
+        :return: list of z_k and list of phi(z_k) as defined in (5) in the paper
+        """
+        z = np.apply_along_axis(lambda x: (y[x[0]]-y[x[1]])/(np.sqrt(2)*self.sigma), 1, self.D)
         return z, np.apply_along_axis(lambda x: norm.cdf(x, loc=0, scale=1), 0, z)
 
-    def compute_S(self,y):
-        phi=self.compute_z_phi(y)[1]
-        prior=0.5*np.inner(y, np.dot(self.inv_cov, y))
+    def compute_S(self, y):
+        """
+        :param y: vector of size n
+        :return: S(y)
+        """
+        phi = self.compute_z_phi(y)[1]
+        prior = 0.5*np.inner(y, np.dot(self.inv_cov, y))
         return -np.sum(np.log(phi))+prior
 
     def s(self, k, i):
         return (i == self.D[k][0])-(i == self.D[k][1])
 
     def compute_grad_S(self, y):
+        """
+        :param y: vector of size n
+        :return: Gradient of S in y
+        """
         z, phi = self.compute_z_phi(y)
-        
+
         def partial_df(k, i):  #for the log_phi part
             return -self.s(k, i)/np.sqrt(2)/self.sigma*norm.pdf(z[k])/phi[k]
-        
+
         phi_grad = np.array([sum([partial_df(k, i) for k in range(self.m)]) for i in range(self.n)])
         return phi_grad+np.dot(self.inv_cov, y)
 
-    def compute_Hessian_S(self,y):
+    def compute_Hessian_S(self, y):
+        """
+        :param y: vector of size n
+        :return: Hessian of S in y
+        """
         z, phi = self.compute_z_phi(y)
-        
+
         def partial_d2f(k, i, j):
             s_ij = self.s(k, i)*self.s(k, j)/2/self.sigma**2
             nk=norm.pdf(z[k])/phi[k]
@@ -73,16 +97,32 @@ class learning_instance_preference:
                               for j in range(self.n)] for i in range(self.n)])
         return phi_Hess+self.inv_cov
 
-    def compute_MAP(self,y):
-        return minimize(self.compute_S, y, method='Newton-CG', jac=self.compute_grad_S, 
+    def compute_MAP(self, y):
+        """
+        :param y: Starting vector for the minimization program
+        :return: A scipy OptimizeResult dict with results after the minimization
+        (convergence, last value, jacobian,...)
+        """
+        return minimize(self.compute_S, y, method='Newton-CG', jac=self.compute_grad_S,
                         hess=self.compute_Hessian_S, tol=1e-4)
 
     def evidence_approx(self, y):
+        """
+        :param y: a vector with n values of f(x) (for x in self.X)
+        :return: Laplace approximation of the evidence of the model with y
+        """
         S, H = self.compute_S(y), self.compute_Hessian_S(y)
-        denom = np.linalg.det(np.dot(self.cov,H))
-        return min(1,np.exp(-S)/np.sqrt(np.abs(denom)))
+        denom = np.linalg.det(np.dot(self.cov, H))
+        return min(1, np.exp(-S)/np.sqrt(np.abs(denom)))
 
     def compute_MAP_with_gridsearch(self, y0, grid_K, grid_sigma):
+        """
+        :param y0: Starting point for optimization
+        :param grid_K: grid for the kernel parameter K
+        :param grid_sigma: grid for the noise variance parameter sigma
+        :return: Maximum a posteriori for the given value of x for the parameters
+        with the largest evidence
+        """
         best_K, best_sigma,best_evidence = grid_K[0], grid_sigma[0], 0.
         for K in tqdm(grid_K):
             for sigma in tqdm(grid_sigma):
@@ -103,7 +143,14 @@ class learning_instance_preference:
         return best_MAP
 
     def predict_single_pref(self, r, s, f_map, M):
-        sigma_t=np.array([[1., self.gaussian_kernel(r, s, self.K)], [self.gaussian_kernel(r, s, self.K), 1.]])
+        """
+        :param r: vector of dimension d
+        :param s: vector of dimension d
+        :param f_map: Maximum a posteriori for n vectors in R^d
+        :param M: covariance matrix computed for the maximum a posteriori
+        :return: Probability that r is preferred over s
+        """
+        sigma_t = np.array([[1., self.gaussian_kernel(r, s, self.K)], [self.gaussian_kernel(r, s, self.K), 1.]])
         kt = np.zeros((self.n, 2))
         kt[:, 0] = [self.gaussian_kernel(r, self.X[i], self.K) for i in range(self.n)]
         kt[:, 1] = [self.gaussian_kernel(s, self.X[i], self.K) for i in range(self.n)]
@@ -112,16 +159,59 @@ class learning_instance_preference:
         new_sigma = np.sqrt(2*self.sigma**2+s_star[0, 0]+s_star[1, 1]-s_star[0, 1]-s_star[1, 0])
         return norm.cdf((mu_star[0]-mu_star[1])/new_sigma, loc=0, scale=1)
 
-    def predict(self,test_set,f_map):
+    def predict(self, test_set, f_map):
+        """
+        :param test_set: tuple of length 2:
+        * an array for the test X from which preferences are drawn
+        * a list of tuples (i,j) for the m' new preferences to predict, where i is
+        preferred to j (helps for the score)
+        :param f_map: Maximum a Posteriori obtained with the Gaussian Kernels method
+        :return: Score of the prediction, list of the probabilities P(i preferred over j)
+        """
         X = test_set[0]
         pref = test_set[1]
         score = 0
         proba_pref = []
         H = self.compute_Hessian_S(f_map)
-        M = np.linalg.inv(self.cov+np.linalg.inv(H-self.cov))
+        M = np.linalg.inv(self.cov+np.linalg.inv(H-self.inv_cov))
         for p in tqdm(pref):
             proba = self.predict_single_pref(X[p[0]], X[p[1]], f_map, M)
             proba_pref.append(proba)
             if proba > 0.5:
                 score += 1
         return score/len(pref), proba_pref
+
+    def get_train_pref(self, mp):
+        """
+        :param mp: Maximum a posteriori for n values of x
+        :return: A matrix with booleans: 1 if map[i]>map[j] else 0
+        """
+        pref = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                if mp[i] > mp[j]:
+                    pref[i, j] = 1
+        return pref
+
+    def get_confusion_matrix(self, pref, map):
+        """
+        :param pref: Boolean matrix with input preferences
+        :param map: Boolean matrix with output preferences
+        :return: confusion matrix (the result is symmetric)
+        """
+        matrix = np.zeros((2, 2))
+        matrix[0, 0] = ((1-pref)*(1-map)).sum()
+        matrix[0, 1] = (pref*(1-map)).sum()
+        matrix[1, 0] = ((1-pref)*map).sum()
+        matrix[1, 1] = (pref*map).sum()
+        return matrix
+
+    def score(self, pref, map):
+        """
+        :param pref: Boolean matrix with input preferences
+        :param map: Boolean matrix with output preferences
+        :return: confusion matrix (the result is symmetric)
+        """
+        tot = self.n*(self.n-1)/2
+        err = ((pref-map) != 0).sum()/2
+        return (tot-err)/tot
--- input_data.py
+++ input_data.py
@@ -1,78 +1,89 @@
 import numpy as np
 import copy
 
+
 class instance_pref_generator:
-    def __init__(self,func,func_param,d,n,m):
-        self.n=n
-        self.m=m
-        self.d=d
-        self.real_f=func
-        self.f_param=func_param
+    def __init__(self, func, func_param, d, n, m):
+        self.n = n
+        self.m = m
+        self.d = d
+        self.real_f = func
+        self.f_param = func_param
 
     def generate_X(self):
-        return np.random.uniform(size=(self.n,self.d))
+        return np.random.uniform(size=(self.n, self.d))
 
     @staticmethod
     def draw_preference(n):
-        a=np.random.randint(n)
-        b=np.random.randint(n)
-        while b==a:
-            b=np.random.randint(n)
-        return a,b
+        a = np.random.randint(n)
+        b = np.random.randint(n)
+        while b == a:
+            b = np.random.randint(n)
+        return a, b
 
-    def add_a_pref(self,X,existing_pref,iter_max=10):
-        a,b=self.draw_preference(self.n)
-        n_iter=0
-        while (a,b) in existing_pref or (b, a) in existing_pref and n_iter<iter_max:
-            a, b=self.draw_preference(self.n)
-            n_iter+=1
-        f_a,f_b=self.real_f(X[a], self.f_param), self.real_f(X[b], self.f_param)
-        if f_a>f_b:
+    def add_a_pref(self, X, existing_pref, iter_max=10):
+        a, b = self.draw_preference(self.n)
+        n_iter = 0
+        while (a, b) in existing_pref or (b, a) in existing_pref and n_iter < iter_max:
+            a, b = self.draw_preference(self.n)
+            n_iter += 1
+        f_a, f_b = self.real_f(X[a], self.f_param), self.real_f(X[b], self.f_param)
+        if f_a > f_b:
             return a, b
         else:
             return b, a
 
-    def set_m_preference(self,X):
-        pref=[]
+    def set_m_preference(self, X):
+        pref = []
         for i in range(self.m):
-            pref.append(self.add_a_pref(X,pref))
+            pref.append(self.add_a_pref(X, pref))
         return pref
 
     def generate_X_pref(self):
-        X=self.generate_X()
-        D=self.set_m_preference(X)
-        return X,D
+        X = self.generate_X()
+        D = self.set_m_preference(X)
+        return X, D
+
+    def get_true_pref(self, X):
+        pref = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.real_f(X[i], self.f_param) > self.real_f(X[j], self.f_param):
+                    pref[i, j] = 1
+        return pref
+
 
 class label_pref_generator(instance_pref_generator):
-    def __init__(self,func,func_param,d,n,m):
-        super().__init__(func,func_param,d,n,m)
-        self.n_label=len(self.real_f)
+    def __init__(self, func, func_param, d, n, m):
+        super().__init__(func,func_param, d, n, m)
+        self.n_label = len(self.real_f)
 
-    def add_a_pref(self,x,existing_pref,iter_max=10):
-        a,b=self.draw_preference(self.n_label)
-        n_iter=0
-        while (a,b) in existing_pref or (b, a) in existing_pref and n_iter<iter_max:
-            a, b=self.draw_preference(self.n_label)
-            n_iter+=1
-        f_a,f_b=self.real_f[a](x, self.f_param[a]), self.real_f[b](x, self.f_param[b])
-        if f_a>f_b:
+    def add_a_pref(self, x, existing_pref, iter_max=10):
+        a, b = self.draw_preference(self.n_label)
+        n_iter = 0
+        while (a, b) in existing_pref or (b, a) in existing_pref and n_iter < iter_max:
+            a, b = self.draw_preference(self.n_label)
+            n_iter += 1
+        f_a, f_b = self.real_f[a](x, self.f_param[a]), self.real_f[b](x, self.f_param[b])
+        if f_a > f_b:
             return a, b
         else:
             return b, a
 
-    def set_m_preference(self,X):
-        '''
+    def set_m_preference(self, X):
+        """
         m becomes the maximum number of edges to draw for each vector in X
-        '''
-        pref=[]
-        n_observed=np.random.randint(low=1,high=self.m+1,size=self.n)
+        """
+        pref = []
+        n_observed = np.random.randint(low=1, high=self.m+1, size=self.n)
         for i in range(self.n):
-            pref_i=[]
+            pref_i = []
             for j in range(n_observed[i]):
-                pref_i.append(self.add_a_pref(X[i],pref_i))
+                pref_i.append(self.add_a_pref(X[i], pref_i))
             pref.append(copy.copy(pref_i))
         return pref
 
+
 def cobb_douglas(x,alpha):
-    x_alpha=x**alpha
+    x_alpha = x**alpha
     return np.prod(x_alpha)
